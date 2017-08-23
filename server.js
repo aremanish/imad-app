830506df4b407daea458bb44f3f8484893592edb
aremanish <aremanish@gmail.com>
2017-08-23 09:19:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,14 +5,54 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+    title:"article-one",
+    content:`   <p>Then, when we started surfing the internet using tablets and mobile phones, fixed size web pages were too large to fit the viewport. To fix this, browsers on those devices scaled down the entire web page to fit the screen1.</p>
+             <p>Then, when we started surfing the internet using tablets and mobile phones, fixed size web pages were too large to fit the viewport. To fix this, browsers on those devices scaled down the entire web page to fit the screen1.</p>
+              <p>Then, when we started surfing the internet using tablets and mobile phones, fixed size web pages were too large to fit the viewport. To fix this, browsers on those devices scaled down the entire web page to fit the screen1.</p>
+              `
+,
+              head:"article-one",
+              date:"Sep 5,2017"
+};
+
+
+function ceateTemplae(obj){
+
+var date=obj.date;
+var head=obj.head;
+var title=obj.title;
+var content=obj.content;
+var html=`
+    <html>
+    <head>
+       <title>{title}</title>
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div><a href="/">Home</a></div>
+        <hr />
+        <h3>{head}</h3>
+        <div>{date}</di0v>
+        <div>
+        {content}         
+        </div>
+    </body>
+</html>
+`
+return html;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
   //res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
-  //res.send("hello a1");
+  //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(ceateTemplae(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
